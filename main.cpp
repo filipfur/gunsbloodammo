d27cc3ffe17cc7be0cbd64e4f74395bd646999e4
filipfur <filipfur@live.se>
2015-11-05 11:41:56 +0100

    first commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 29 okt 2015
+ *      Author: filfu175
+ */
+
+#include "Game.h"
+
+int main(int argc, char* argv[]){
+
+	Game game;
+	int error = game.run(640, 480);
+
+	return error;
+}
+
+
+
+
